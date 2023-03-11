4ae59bacc3a1f38513681ed0f8da64465eb3b7ed
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2023-03-11 14:48:30 -0300

    first commit

---
+++ usr/share/bigbashview/bcc/apps/bigbashview-calamares/desktops/xivastudio/desc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##################################
+#  Author1: Bruno Goncalves (www.biglinux.com.br) 
+#  Author2: Rafael Ruscher (rruscher@gmail.com)  
+#  Author3: Barnabé di Kartola (barnabedikartola@gmail.com)  
+#  Date:    2022/02/28 
+#  Modified:2022/12/06 
+#  
+#  Description: Calamares modified usage of BigLinux 
+#  
+# Licensed by GPL V2 or greater
+##################################
+
+#Translation
+LANGUAGE=$LANG:en
+export TEXTDOMAINDIR="/usr/share/locale"
+export TEXTDOMAIN=biglinux-calamares-xivastudio
+
+
+DESKTOP_NAME_DESC=$"XIVA Studio"
+DESKTOP_INFO_DESC=$"BigLinux para criação Multi-Media."
